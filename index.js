709cc029411611693b456477d2708c43c820e3d1
kiwaeva <kiwaeva@gmail.com>
2025-10-05 13:52:37 +0100

    Create reset button with popup to create new grid with different number of squares that is < 100.

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ const container = document.querySelector('#container')
     container.appendChild(newDiv);
   }
 
+//create a popup to ask for a number
+
+const button = document.querySelector('#button');
+
+button.addEventListener('click', (e) => {
+    const num = parseInt(prompt('What number of squares do you want in your new grid?'));
+
+    if (isNaN(num) || num <= 0) {
+    alert('Please enter a valid positive number');
+    return;
+}
+
+//Clear existing grid
+container.innerHTML = "";
+
+for (let i = 0; i < num; i++){
+    const createGrid = document.createElement('div');
+    createGrid.classList.add('grid-square');
+    createGrid.textContent = i + 1;
+
+    createGrid.addEventListener('mouseenter',(e) =>{
+        e.target.classList.add('color-change');
+    });
+
+        container.appendChild(createGrid);
+    }
+});
